7eb154222d542013ff24a6c3f1a6c9d19c6bc7f7
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-10-14 20:17:04 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -5,7 +5,7 @@ badge_maker('jessica')
 
 def batch_badge_creator(name)
  name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
- array.each<< ";presenter"
+ array<< ";presenter"
  puts"Hello, my name is #{name}."
  end
  batch_badge_creator(name)

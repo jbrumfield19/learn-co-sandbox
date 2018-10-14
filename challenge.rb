6c507b673682e00210598baa4950f4a3093027e5
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-10-14 20:54:33 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -4,10 +4,10 @@ end
 badge_maker('jessica')
 
 def batch_badge_creator(name)
-  name=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  puts "Hello, my name is #{name}."
+  people=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts "Hello, my name is #{people}."
 end
-  batch_badge_creator(name)
+  batch_badge_creator(people)
   
   def assign_room(person,room)
     room=[1,2,3,4,5,6,7]

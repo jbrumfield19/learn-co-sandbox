5a7a204a25f87d538f721e81c52c8ca3118b86dd
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-10-14 20:42:48 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -4,7 +4,7 @@ end
 badge_maker('jessica')
 
 def batch_badge_creator(name)
-  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 end
   batch_badge_creator
